6314a16545bab00b16e804982b1c54485ca55135
Lewis Luckhurst <lewisluckhurst@icloud.com>
2020-11-14 18:04:21 +0000

    adding region selection button

--- src/components/search-view/Search.js
+++ src/components/search-view/Search.js
@@ -5,6 +5,10 @@ import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 import "./SearchView.css";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,29 +23,51 @@ const useStyles = makeStyles(theme => ({
     iconButton: {
         padding: 10
     },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
 }));
 
 function SearchBar(props) {
     const classes = useStyles();
     const [summonerName, setSummonerName] = useState("");
+    const [region, setRegion] = useState("");
+
+    const handleChange = (event) => {
+        setRegion(event.target.value);
+    };
 
     return (
-        <Paper className={classes.root}>
-            <InputBase
-                className={classes.input}
-                placeholder="Enter summoner name"
-                inputProps={{"aria-label": "search force graph"}}
-                onChange={(e) => setSummonerName(e.target.value)}
-            />
-            <IconButton
-                className={classes.iconButton}
-                aria-label="search"
-                onClick={() => props.updateSummonerObject(summonerName)}
-            >
-                <SearchIcon/>
-            </IconButton>
-        </Paper>
-    );
+        <>
+            <Paper className={classes.root}>
+                <InputBase
+                    className={classes.input}
+                    placeholder="Enter summoner name"
+                    inputProps={{"aria-label": "search force graph"}}
+                    onChange={(e) => setSummonerName(e.target.value)}
+                />
+                <IconButton
+                    className={classes.iconButton}
+                    aria-label="search"
+                    onClick={() => props.updateSummonerObject(summonerName)}
+                >
+                    <SearchIcon/>
+                </IconButton>
+            </Paper>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Region</InputLabel>
+                <Select
+                    value={region}
+                    onChange={handleChange}
+                >
+                    <MenuItem value={"EUW"}>EUW</MenuItem>
+                    <MenuItem value={"NA"}>NA</MenuItem>
+                </Select>
+            </FormControl>
+        </>
+    )
+        ;
 }
 
 export default SearchBar;
